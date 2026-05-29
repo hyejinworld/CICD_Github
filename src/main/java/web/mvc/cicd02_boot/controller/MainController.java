e7ec57c21b2dc20b4791c5bba476000e22f072cc
hyejinworld <amorfati7350@gmail.com>
2026-05-29 16:09:35 +0900

    yml수정

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 github second CI/CD TEST-자동배포";
+        return "GitHub Actions를 이용한 github yml수정-자동배포";
     }
 }
